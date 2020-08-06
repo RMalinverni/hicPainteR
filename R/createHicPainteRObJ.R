b00d822c7858af57f0c10e468d8a0986cb964d40
RMalinverni <roberto.malinverni@gmail.com>
2020-08-06 11:59:53 +0200

    add manual to createHicPainteROBj

--- R/createHicPainteRObJ.R
+++ R/createHicPainteRObJ.R
@@ -1,14 +1,48 @@
-#create an "HicPainteRObj" object that will be recognize using a from karyoploteR
+#' Create an hicPainteR object
+#'
+#' create an hicPainteR object starting from a GenomicInteractions object
+#'
+#' @usage createHicPainteRObj((contact,mapType,Name,zoom=NA,high_fact=1e6,pCol="red",alpha=0.05,enhance=NA,colBin=500,
+#'        use_ramp=FALSE,ramp=NA,log=FALSE,pchP=".",lwdP=2,ltyP=1,cexP=2,scores=NA,use.scores=FALSE,...))
+#'
+#' @param contact GenomicInteractions object
+#' @param mapType character - possible choises are cMAP, TAD, Loops
+#' @param Name character - is the name of the object ( default NA )
+#' @param high_fact numeric - value for manage the proportion of graph ( default 1e6 ) (TODO control if making it automatic)
+#' @param pCol charachter - color used if \code use_ramp == FALSE ( default "red" )
+#' @param alpha numeric (0-1) - is the value used for the transparency, mutuated from \code{\link{ggplot2} (default 0.05)
+#' @param enhance numeric - transform factor for counts on cMap ( default = NA )
+#' @param colBin numeric - used to calculate the number of the palette when use_ramp == TRUE ( default  500 )
+#' @param use_ramp logic - select to switch from flat color to \code colorRampPalette ( default FALSE )
+#' @param ramp colorRampPalette object - palette used when use_ramp == TRUE, when is set to NA ramp= ( default NA )
+#' @param log logic - transform the counts of the contact object in logaritmic ( default NA )
+#' @param pchP character - pch code for points drawing
+#' @param lwdP numeric - lwd thick for line drawing
+#' @param ltyP numeric - lty code for line drawing
+#' @param cexP numeric - cex value for point drawing
+#'
+#'
+#'
+#' @seealso ...
+#'
+#' @examples  ...
+#'
+#' #@references ...
+#' #@importFrom ...
+#' @export createHicPainteRObj
 
-createHicPainteRObj<-function(Name,contact,mapType,zoom=NA,high_fact=1e6,pCol="red",alpha=0.05,enhance=NA,colBin=500,
-                              use_ramp=FALSE,log=FALSE,ramp=NA,pchP=".",cexP=2,lwdP=2,ltyP=1,scores=NA,use.scores=FALSE,...){
+createHicPainteRObj<-function(contact, mapType, Name=NA, zoom=NA, high_fact=1e6, pCol="red", alpha=0.05, enhance=NA,
+                              colBin=500, use_ramp=FALSE, ramp=NA, log=FALSE, pchP=".", cexP=2, lwdP=2, ltyP=1,
+                              scores=NA, use.scores=FALSE, ...){
 
 
   if (mapType %in% c('cMap','TAD','Loop') == FALSE ){
     stop ('"Type" need to be one of the follows: Hic, TAD or Loop')
   }
 
-
+  if( is.na(Name) ){
+    Name<-paste0(mapType,"_hP")
+  }
   #contact add an error
   #ramp
   #pchP
