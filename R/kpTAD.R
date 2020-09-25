ed88766d53f494744f455fd2e05b4231bf8123b3
RMalinverni <roberto.malinverni@gmail.com>
2020-09-25 09:40:16 +0200

    kpTAD version

--- R/kpTAD.R
+++ R/kpTAD.R
@@ -1,4 +1,4 @@
-kpTAD<-function(kp,hicPobj,r0=0,r1=1){
+
 kpTAD<-function(kp,hicPobj,r0=0,r1=1,colorTAD="black",lim_dist = 1e6){
 
   if  (!is.na(lim_dist)){
@@ -8,12 +8,9 @@ kpTAD<-function(kp,hicPobj,r0=0,r1=1,colorTAD="black",lim_dist = 1e6){
   chr=seqlevels(hicPobj$cMap[1])
   kpSegments(kp,chr=chr,x0=start(hicPobj$cMap),x1=hicPobj$cMap$edge,
              y0=0,y1=hicPobj$cMap$Ys,r0=r0,
-             r1=r1,col="black",lwd=hicPobj$param$lwdP,lty=hicPobj$param$ltyP)
              r1=r1,col=colorTAD,lwd=hicPobj$param$lwdP,lty=hicPobj$param$ltyP)
   kpSegments(kp,chr=chr,x0=hicPobj$cMap$edge,x1=end(hicPobj$cMap),
              y0=hicPobj$cMap$Ys,y1=0,r0=r0,
-             r1=r1,col="black",lwd=hicPobj$param$lwdP,lty=hicPobj$param$ltyP)
-}
              r1=r1,col=colorTAD,lwd=hicPobj$param$lwdP,lty=hicPobj$param$ltyP)
   }
 
