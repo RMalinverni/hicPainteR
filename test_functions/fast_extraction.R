75f75b04d5aa18079a6c0230c277d50a6e3cc510
RMalinverni <roberto.malinverni@gmail.com>
2020-10-13 17:00:49 +0200

    playing around fast extraction

--- test_functions/fast_extraction.R
+++ test_functions/fast_extraction.R
@@ -4,6 +4,9 @@ library(GenomicInteractions)
 library(plyranges)
 library(tidyverse)
 library(karyoploteR)
+library(RColorBrewer)
+library("viridis")
+
 
 TAD<-
 zoomString<-"chr2:175,849,978-178,182,899"
@@ -80,27 +83,75 @@ DFfromHIC<-function(hicFile,zoom,seq_style="UCSC",bin=10000,norm="KR"){
   return(hic.data.frame)
 }
 
-hic.data.frame <- strawr::straw("KR", "/path/to/file.hic", "11", "11", "BP", 10000)
+#hic.data.frame <- strawr::straw("KR", "/path/to/file.hic", "11", "11", "BP", 10000)
+zoom<-createZooms(zoomString)
 
-hic.file <- "/home/malli/work/insulation/hichip/K4_hichip_q_10.hic"
-Dump <- DFfromHIC(hicFile = hic.file,seq_style = "NCBI",zoom = zoomString,norm = "NONE")
+
+hic.file <- "/home/rmalinverni/shares/BDbuschbeck/rmalinverni/07_Biodata/hic/ENCODE_hic_hepg2.chr2.hic"
+Dump <- DFfromHIC(hicFile = hic.file,seq_style = "NCBI",zoom = zoom$zoom, norm = "KR")
 GI <- hicDump2GI(Dump,chr=zoom$chr)
 HiCO <- createHicPainteRObj(GI,mapType = "cMap")
 
-zoom<-createZooms(zoomString)
+
 kp <-plotKaryotype(zoom = zoom$GR)
 kpHiC(kp,HiCO)
 
+
+
 TAD<-regioneR::toGRanges(data.frame(chr="chr2",start=min(start(gInteractionsTocMap(GI))),end=max(start(gInteractionsTocMap(GI)))))
-selGR<-reShapeTAD(TAD,gInteractionsTocMap(GI), xbin=10000)
+selGR<-reShapeTAD(TAD,gInteractionsTocMap(GI), xbin=1000)
+fakeGenome<-regioneR::toGRanges(data.frame(chr="ChrU",start=1,end=1000))
+XX<-as.data.frame(xtabs(log(selGR$counts)~selGR$x1 +selGR$y))
+#XX<- XX %>% filter(Freq !=0)
+kp<-plotKaryotype(genome = fakeGenome)
+
+kpPoints(kp,
+         x=as.numeric(as.character(XX$selGR.x1)),
+         y = as.numeric(as.character(XX$selGR.y)),
+         chr = "ChrU",
+         col=alpha(alpha=rescale(log1p(XX$Freq)),col="red"),pch=18,cex=3)
+
+image(log1p(as.matrix(as.data.frame.matrix(xtabs(log(selGR$counts)~selGR$x1 +selGR$y)))))
+
+image(xtabs(log(selGR$counts)~selGR$x1 +selGR$y), axes=FALSE, zlim=c(-2,2), col=ramp(1000))
+
+as.data.frame.matrix(xtabs(log(selGR$counts)~selGR$x1 +selGR$y))
+colsR<-round(rescale((XX$Freq),to=c(0,100)))
+
+ramp<-colorRampPalette(c("white","cadetblue","red","orange","yellow","yellow","yellow"))
+
+TAD<-regioneR::toGRanges(data.frame(chr="chr2",start=min(start(gInteractionsTocMap(GI))),end=max(start(gInteractionsTocMap(GI)))))
+selGR<-reShapeTAD(TAD,gInteractionsTocMap(GI), xbin=100)
 
 zoomString<-"chr2:175,849,978-178,182,899"
-XX<-xtabs(log(selGR$counts)~selGR$x1 +selGR$y)
+
+library(lattice)
+
+levelplot(xtabs(log1p(selGR$counts)~selGR$x1 +selGR$y),col.regions=ramp(100))
+
+
+selGR<-reShapeTAD(TAD,gInteractionsTocMap(GI), xbin=500,yfactor=2)
+rsc<-(xtabs(selGR$counts~selGR$x1 +selGR$y))
+rsc<-as.matrix(as.data.frame.matrix(rsc))
+levelplot(log1p(rsc),col.regions=ramp(1000))
 
 
 
+
+
+df<-data.frame(C1=c(1,1,1,0,2),
+               C2=c(2,2,2,3,3),
+               C3=c(10,5,1,0,0))
+XT1<-xtabs(df$C3~df$C1 + df$C2)
+
+df2<-data.frame(C1=c(1,2,3,4,5),
+               C2=c(1,2,3,4,5),
+               C3=c(1,5,1,0,0))
+XT2<-xtabs(df2$C3~df2$C1 + df2$C2)
 mat<-as.matrix(XX)
 image(log(as.matrix(XX)))
+XT1<-as.matrix(as.data.frame.matrix(XT1))
+XT2<-as.matrix(as.data.frame.matrix(XT2))
 
-
+(XT2,XT1)
 
